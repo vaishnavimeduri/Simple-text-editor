ce1ca0d67f0d808a15c03c28007dd741ef96e63f
vaishnavimeduri <meduri.vaishu676@gmail.com>
2022-04-28 19:50:54 +0530

    formatted

--- main.java
+++ main.java
@@ -1,12 +1,8 @@
 public class main {
-public static void main(String[] args) {
-       
-    new textEditor();
-       
-   }
+    public static void main(String[] args) {
+
+        new textEditor();
+
+    }
 
 }
-
-
-
-
--- textEditor.java
+++ textEditor.java
@@ -31,62 +31,62 @@ public class textEditor extends JFrame implements ActionListener {
     JMenuItem redoItem;
     UndoManager undo1 = new UndoManager();
 
-    textEditor(){
+    textEditor() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Simple Text Editor");
-        this.setSize(500,500);
+        this.setSize(500, 500);
         this.setLayout(new FlowLayout());
         this.setLocationRelativeTo(null);
 
-        textarea=new JTextArea();
-       // textarea.setPreferredSize(new Dimension(450,450));
-       textarea.getDocument().addUndoableEditListener(
+        textarea = new JTextArea();
+        // textarea.setPreferredSize(new Dimension(450,450));
+        textarea.getDocument().addUndoableEditListener(
                 new UndoableEditListener() {
                     public void undoableEditHappened(UndoableEditEvent e1) {
                         undo1.addEdit(e1.getEdit());
-                }
-});
+                    }
+                });
         textarea.setLineWrap(true);
         textarea.setWrapStyleWord(true);
-        textarea.setFont(new Font("Arial",Font.PLAIN,20));
+        textarea.setFont(new Font("Arial", Font.PLAIN, 20));
 
-        scrollPane=new JScrollPane(textarea);
-        scrollPane.setPreferredSize(new Dimension(450,450));
+        scrollPane = new JScrollPane(textarea);
+        scrollPane.setPreferredSize(new Dimension(450, 450));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
-        fontLabel=new JLabel("Font: ");
+        fontLabel = new JLabel("Font: ");
 
-        fontSizeSpinner=new JSpinner();
-        fontSizeSpinner.setPreferredSize(new Dimension(50,25));
+        fontSizeSpinner = new JSpinner();
+        fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
         fontSizeSpinner.setValue(20);
         fontSizeSpinner.addChangeListener(new ChangeListener() {
             @Override
-            public void stateChanged(ChangeEvent e){
-                textarea.setFont(new Font(textarea.getFont().getFamily(),Font.PLAIN,(int)fontSizeSpinner.getValue()));
+            public void stateChanged(ChangeEvent e) {
+                textarea.setFont(
+                        new Font(textarea.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
             }
         });
 
-        fontColorButton=new JButton("Color");
+        fontColorButton = new JButton("Color");
         fontColorButton.addActionListener(this);
 
-        String[] fonts =GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
 
-        fontBox=new JComboBox(fonts);
+        fontBox = new JComboBox(fonts);
         fontBox.addActionListener((this));
         fontBox.setSelectedItem("Arial");
 
-        menuBar=new JMenuBar();
+        menuBar = new JMenuBar();
 
-        fileMenu=new JMenu("File");
+        fileMenu = new JMenu("File");
 
-        openItem=new JMenuItem("Open");
-        saveItem=new JMenuItem("Save");
-        exitItem=new JMenuItem("Exit");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
 
         openItem.addActionListener(this);
         saveItem.addActionListener(this);
         exitItem.addActionListener(this);
-
 
         fileMenu.add(openItem);
         fileMenu.add(saveItem);
@@ -94,24 +94,20 @@ public class textEditor extends JFrame implements ActionListener {
 
         menuBar.add(fileMenu);
 
-        editmenu=new JMenu("Edit");
+        editmenu = new JMenu("Edit");
 
-        undoItem=new JMenuItem("Undo");
-       redoItem=new JMenuItem("Redo");
-        
+        undoItem = new JMenuItem("Undo");
+        redoItem = new JMenuItem("Redo");
 
         undoItem.addActionListener(this);
         redoItem.addActionListener(this);
-       
 
-
-       editmenu.add(undoItem);
-       editmenu.add(redoItem);
-        
+        editmenu.add(undoItem);
+        editmenu.add(redoItem);
 
         menuBar.add(editmenu);
 
-        this.setJMenuBar(menuBar); 
+        this.setJMenuBar(menuBar);
         this.add(fontLabel);
         this.add(fontSizeSpinner);
         this.add(fontColorButton);
@@ -119,84 +115,82 @@ public class textEditor extends JFrame implements ActionListener {
         this.add(scrollPane);
         this.setVisible(true);
     }
+
     @Override
-    public void actionPerformed(ActionEvent e){
-      if(e.getSource()==fontColorButton){
-          JColorChooser colorChooser=new JColorChooser();
-          Color color=colorChooser.showDialog(null, "choose a color", Color.BLACK);
-          textarea.setForeground(color);
-      }
-      if(e.getSource()==fontBox){
-          textarea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN,textarea.getFont().getSize()));
-      }
-      if(e.getSource()==openItem){
-        JFileChooser fileChooser=new JFileChooser();
-        fileChooser.setCurrentDirectory(new File(".")); 
-        FileNameExtensionFilter filter=new FileNameExtensionFilter("Text files","txt");
-        fileChooser.setFileFilter(filter);
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == fontColorButton) {
+            JColorChooser colorChooser = new JColorChooser();
+            Color color = colorChooser.showDialog(null, "choose a color", Color.BLACK);
+            textarea.setForeground(color);
+        }
+        if (e.getSource() == fontBox) {
+            textarea.setFont(new Font((String) fontBox.getSelectedItem(), Font.PLAIN, textarea.getFont().getSize()));
+        }
+        if (e.getSource() == openItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+            fileChooser.setFileFilter(filter);
 
-        int response=fileChooser.showOpenDialog(null);
+            int response = fileChooser.showOpenDialog(null);
 
-        if(response==JFileChooser.APPROVE_OPTION){
-            File file=new File(fileChooser.getSelectedFile().getAbsolutePath());
-            Scanner fileIn =null;
-           try {
-            fileIn =new Scanner(file);
-            if(file.isFile()){
-                while(fileIn.hasNextLine()){
-                    String line=fileIn.nextLine()+"\n";
-                    textarea.append(line);
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+                try {
+                    fileIn = new Scanner(file);
+                    if (file.isFile()) {
+                        while (fileIn.hasNextLine()) {
+                            String line = fileIn.nextLine() + "\n";
+                            textarea.append(line);
+                        }
+                    }
+                } catch (FileNotFoundException e1) {
+
+                    e1.printStackTrace();
+                } finally {
+                    fileIn.close();
                 }
             }
-           } catch (FileNotFoundException e1) {
-               
-               e1.printStackTrace();
-           }
-           finally{
-               fileIn.close();
-           }
+
         }
+        if (e.getSource() == saveItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            int response = fileChooser.showSaveDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file;
+                PrintWriter fileOut = null;
 
-      }
-      if(e.getSource()==saveItem){
-         JFileChooser fileChooser=new JFileChooser();
-         fileChooser.setCurrentDirectory(new File(".")); 
-         int response=fileChooser.showSaveDialog(null);
-         if(response==JFileChooser.APPROVE_OPTION){
-             File file;
-             PrintWriter fileOut=null;
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textarea.getText());
+                } catch (FileNotFoundException e1) {
 
-             file=new File(fileChooser.getSelectedFile().getAbsolutePath());
-             try {
-                fileOut=new PrintWriter(file);
-                fileOut.println(textarea.getText());
-             } 
-             catch (FileNotFoundException e1) {
-                
-                 e1.printStackTrace();
-             }
-             finally{
-                 fileOut.close();
-             }
-            
-         }
-    }
-    if(e.getSource()==exitItem){
-          System.exit(0);
-    }
-    if(e.getSource()==undoItem){
-        try {
-            undo1.undo();
-         } catch (CannotRedoException cre) {
-            cre.printStackTrace();
-         }
-    }
-    if(e.getSource()==redoItem){
-        try {
-            undo1.redo();
-         } catch (CannotRedoException cre) {
-            cre.printStackTrace();
-         }
-    }
+                    e1.printStackTrace();
+                } finally {
+                    fileOut.close();
+                }
+
+            }
+        }
+        if (e.getSource() == exitItem) {
+            System.exit(0);
+        }
+        if (e.getSource() == undoItem) {
+            try {
+                undo1.undo();
+            } catch (CannotRedoException cre) {
+                cre.printStackTrace();
+            }
+        }
+        if (e.getSource() == redoItem) {
+            try {
+                undo1.redo();
+            } catch (CannotRedoException cre) {
+                cre.printStackTrace();
+            }
+        }
     }
 }
